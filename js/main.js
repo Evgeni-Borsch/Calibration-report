b53783e14b607d3e6cdcc8a0e91e0b82e16bf627
unknown <borschewsky@gmail.com>
2021-02-28 06:06:39 +0300

    first

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var btnSearch=document.getElementById('btn__search');
+
+btnSearch.onclick = function () {
+    var search=document.querySelector('.About');
+    search.style.display='block';
+}
+
+var btn=document.getElementById('btn__report');
+
+btn.onclick = function () {
+    var table=document.querySelector('.Table');
+    table.style.display='block';
+}
